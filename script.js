9fa0039629ec985df440a308bfcab36d95272746
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2022-05-05 18:38:24 +0900

    通知を追加してみる

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 (function () {
+  const title = "my-pwa-sandbox"
   const incrementElem = document.querySelector("#increment");
   const countElem = document.querySelector("#count");
+  const notification1ButtonElem = document.querySelector("#notification1Button");
+  const notification2ButtonElem = document.querySelector("#notification2Button");
+
+  async function isEnabledNotification() {
+    if (Notification.permission === "granted") {
+      return true;
+    }
+    const result = await Notification.requestPermission();
+    return result === "granted";
+  }
 
   incrementElem.addEventListener("click", () => {
     let count = Number(countElem.textContent || 0);
@@ -8,13 +19,53 @@
     countElem.textContent = count;
   });
 
+  notification1ButtonElem.addEventListener("click", async () => {
+    if (!(await isEnabledNotification())) {
+      return;
+    }
+    const serviceWorkerRegistration = await navigator.serviceWorker.ready
+    serviceWorkerRegistration.showNotification(title, {
+      body: "通知のテストです",
+      //image: '/notification.png',やたら大きいイラスト通知エリアにでてくる
+      icon: "/notification.png",
+    });
+  });
+
+  notification2ButtonElem.addEventListener("click", async () => {
+    if (!(await isEnabledNotification())) {
+      return;
+    }
+
+    const serviceWorkerRegistration = await navigator.serviceWorker.ready
+    let counter = 0;
+    const timerId = setInterval(() => {
+      counter++;
+      if (counter > 5) {
+        clearInterval(timerId);
+        return;
+      }
+
+      serviceWorkerRegistration.showNotification(title, {
+        body: `通知のテストです[${counter}]`,
+        icon: "/notification.png",
+        tag: "button2", //タグをつけると グルーピングできるっぽい,
+        renotify: true, //タグ付けた場合はONにしておいたほうがいいかも、じゃないと古いのが画面に残る
+      });
+    }, 1500);
+
+  });
+
+  //-----------------
+
   if ("serviceWorker" in navigator) {
-    const isLocal = location.hostname === 'localhost'
-    console.log('isLocal', isLocal)
+    const isLocal = location.hostname === "localhost";
+    console.log("isLocal", isLocal);
     if (isLocal) {
       navigator.serviceWorker.register("./sw.js");
     } else {
-      navigator.serviceWorker.register("./sw.js", {scope: '/my-pwa-sandbox/'});
+      navigator.serviceWorker.register("./sw.js", {
+        scope: "/my-pwa-sandbox/",
+      });
     }
   }
 })();
